3dfada22083ebbc42da729411b148462a54bf890
Zilvinas <zverseckas@gmail.com>
2017-12-14 21:10:09 +0200

    Error handler as a function

--- lib/require_version.ex
+++ lib/require_version.ex
@@ -2,13 +2,13 @@ defmodule ApiVersioner.RequireVersion do
   import Plug.Conn
 
   def init(opts) do
-    %{error_handler: opts[:error_handler] || ApiVersioner.ErrorHandler}
+    %{error_handler: opts[:error_handler] || &ApiVersioner.ErrorHandler.call/1}
   end
 
   def call(%Plug.Conn{assigns: %{version: _version}} = conn, _opts), do: conn
   def call(conn, opt), do: handle_error(conn, opt.error_handler)
 
   defp handle_error(conn, error_handler) do
-    apply(error_handler, :call, [halt(conn)])
+    error_handler.(halt(conn))
   end
 end
