2d550f1deb1556ed4b27717fd4080597c9c01ef9
zverseckas <zverseckas@gmail.com>
2017-12-22 15:09:50 +0200

    Tabs to spaces

--- test/set_version_test.exs
+++ test/set_version_test.exs
@@ -9,56 +9,56 @@ defmodule SetVersionTest do
   doctest SetVersion
 
   test "creates a valid options map" do
-  	opts = SetVersion.init([])
+    opts = SetVersion.init([])
     assert Map.keys(opts) === [:accepts, :default, :header]
   end
 
   test "default header is Accept" do
-  	%{header: header} = SetVersion.init([])
+    %{header: header} = SetVersion.init([])
     assert header === "accept"
   end
 
   setup _opts do
-  	{:ok,
-  		[
-		  	no_default: SetVersion.init(
-		  		accepts: ~w(test1 test2)a
-		  	),
-		  	with_default: SetVersion.init(
-		  		accepts: ~w(test1 test2)a,
-		  		default: :test1
-	  		)
-	  	]
-		}
+    {:ok,
+      [
+        no_default: SetVersion.init(
+          accepts: ~w(test1 test2)a
+        ),
+        with_default: SetVersion.init(
+          accepts: ~w(test1 test2)a,
+          default: :test1
+        )
+      ]
+    }
   end
 
   test "sets version when a valid one provided", opts do
     with_mock MIME, extensions: fn ("Test1") -> [:test1] end do
-    	conn =
-    		conn(:get, "/", "")
-  	  	|> put_req_header("accept", "Test1")
-  	  	|> SetVersion.call(opts[:no_default])
+      conn =
+        conn(:get, "/", "")
+        |> put_req_header("accept", "Test1")
+        |> SetVersion.call(opts[:no_default])
 
-  	  assert conn.assigns.version === :test1
+      assert conn.assigns.version === :test1
     end
   end
 
   test "does not set version when not provided", opts do
-  	conn =
-  	 conn(:get, "/", "")
-  	 |> SetVersion.call(opts[:no_default])
+    conn =
+     conn(:get, "/", "")
+     |> SetVersion.call(opts[:no_default])
 
-  	refute conn.assigns[:version]
+    refute conn.assigns[:version]
   end
 
    test "sets version when not provided but default exists", opts do
     with_mock MIME, has_type?: fn(:test1) -> true end,
                     extensions: fn(_type) -> [] end] do
-    	conn =
-    	 conn(:get, "/", "")
-    	 |> SetVersion.call(opts[:with_default])
+      conn =
+       conn(:get, "/", "")
+       |> SetVersion.call(opts[:with_default])
 
-    	assert conn.assigns.version === :test1
+      assert conn.assigns.version === :test1
     end
   end
 end
