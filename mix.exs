70cd8cbc8304c731fdf4eb630de10097d3f9ae76
Žilvinas Verseckas <zverseckas@gmail.com>
2018-01-06 18:47:14 +0200

    Elixir version downgrade

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ApiVersioner.Mixfile do
     [
       app: :api_versioner,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.3",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       preferred_cli_env: [
